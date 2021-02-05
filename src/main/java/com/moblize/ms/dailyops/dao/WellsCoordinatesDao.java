34f500e27de944c871bd5555781750d082e2844d
Brian Hettiger <brianhettiger0219@gmail.com>
2021-02-05 19:04:30 +0530

    MSB-4027 - Now updates all the fields of WellSurveyPlannedLatLong

--- src/main/java/com/moblize/ms/dailyops/dao/WellsCoordinatesDao.java
+++ src/main/java/com/moblize/ms/dailyops/dao/WellsCoordinatesDao.java
@@ -74,13 +74,9 @@ public class WellsCoordinatesDao {
     }
 
     public WellSurveyPlannedLatLong updateWellSurveyPlannedLatLong(WellSurveyPlannedLatLong wellSurveyPlannedLatLong) {
-        WellSurveyPlannedLatLong dbObj = findWellSurveyPlannedLatLong(wellSurveyPlannedLatLong.getUid());
-        if (null != wellSurveyPlannedLatLong.getDrilledData() && !wellSurveyPlannedLatLong.getDrilledData().isEmpty()) {
-            if (null != dbObj.getDrilledData()) {
-                dbObj.getDrilledData().addAll(wellSurveyPlannedLatLong.getDrilledData());
-            }
-        }
-        return wellSurveyPlannedLatLongRepository.save(dbObj);
+        final WellSurveyPlannedLatLong dbObj = findWellSurveyPlannedLatLong(wellSurveyPlannedLatLong.getUid());
+        wellSurveyPlannedLatLong.set_id(dbObj.get_id());
+        return wellSurveyPlannedLatLongRepository.save(wellSurveyPlannedLatLong);
     }
 
     public WellSurveyPlannedLatLong findWellSurveyPlannedLatLong(String uid) {
