c43475df804ab5958cafaef88d78c0b574a5c1e1
Brian Hettiger <brianhettiger0219@gmail.com>
2021-03-26 20:55:50 +0530

    MSB-4155
    Removed unused import and set null if data does not exist

--- src/main/java/com/moblize/ms/dailyops/dto/Section.java
+++ src/main/java/com/moblize/ms/dailyops/dto/Section.java
@@ -1,14 +1,9 @@
 
 package com.moblize.ms.dailyops.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @JsonPropertyOrder({
     "a",
