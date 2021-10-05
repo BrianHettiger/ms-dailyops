90dd4e4178efb2d11f2de44dc4e08b9d6b47a390
Brian Hettiger <brianhettiger0219@gmail.com>
2021-10-05 19:41:33 +0530

    MSB-4778
    added EnableAsync

--- src/main/java/com/moblize/ms/dailyops/service/NotifyDPVAService.java
+++ src/main/java/com/moblize/ms/dailyops/service/NotifyDPVAService.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,6 +24,7 @@ import java.util.Optional;
 
 @Service
 @Slf4j
+@EnableAsync
 public class NotifyDPVAService {
     @Autowired
     @Lazy
