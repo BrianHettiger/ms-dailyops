a3a914e055ea5d073a9b75b07b97d5a94373e8b6
Brian Hettiger <brianhettiger0219@gmail.com>
2021-10-29 21:31:31 +0530

    MSB-4490 BCW Smoothing implementation & bug fixes.

--- src/main/java/com/moblize/ms/dailyops/service/BCWDepthLogPlotService.java
+++ src/main/java/com/moblize/ms/dailyops/service/BCWDepthLogPlotService.java
@@ -105,7 +105,7 @@ public class BCWDepthLogPlotService {
                 }
             }
 
-            if(bcwDepthLog.isEmpty()){
+            if(bcwDepthLog.isEmpty() && (bcwDepthPlotDTO.getBcwId() == null || bcwDepthPlotDTO.getBcwId().isEmpty())){
                 log.debug("No Smoothing data found for well UID: {} Hence it's going to get sampled data",
                     bcwDepthPlotDTO.getPrimaryWellUid());
 
