c0762dc12e31eb9cf29bc46f7608f26d21f0ed4d
Brian Hettiger <brianhettiger0219@gmail.com>
2021-12-14 00:23:22 +0530

    MSB-4899 - Make the Performance Map data process first then DPVA

--- src/main/java/com/moblize/ms/dailyops/service/CacheService.java
+++ src/main/java/com/moblize/ms/dailyops/service/CacheService.java
@@ -1,14 +1,18 @@
 package com.moblize.ms.dailyops.service;
 
 import com.moblize.ms.dailyops.domain.MongoWell;
-import com.moblize.ms.dailyops.domain.mongo.WellPerformanceMetaData;
 import com.moblize.ms.dailyops.domain.mongo.DailyOpsLoadConfig;
 import com.moblize.ms.dailyops.dto.TrueRopCache;
 import com.moblize.ms.dailyops.dto.WellCoordinatesResponseV2;
 import com.moblize.ms.dailyops.repository.mongo.client.DPVALoadConfigRepository;
 import com.moblize.ms.dailyops.repository.mongo.client.WellPerformanceMetaDataRepository;
 import com.moblize.ms.dailyops.repository.mongo.mob.MongoWellRepository;
-import com.moblize.ms.dailyops.service.dto.*;
+import com.moblize.ms.dailyops.service.dto.PlannedPerFeetDTO;
+import com.moblize.ms.dailyops.service.dto.SurveyCacheDTO;
+import com.moblize.ms.dailyops.service.dto.SurveyPerFeetDTO;
+import com.moblize.ms.dailyops.service.dto.TargetWindowPerFootDTO;
+import com.moblize.ms.dailyops.service.dto.TortuosityDTO;
+import com.moblize.ms.dailyops.service.dto.WellPlanCacheDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.infinispan.client.hotrod.DefaultTemplate;
 import org.infinispan.client.hotrod.RemoteCache;
@@ -20,8 +24,6 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
-import java.util.Collection;
 
 @Service
 @Slf4j
@@ -56,6 +58,9 @@ public class CacheService {
 
         getWellCoordinatesCache().clear();
         log.info("Cache service start");
+        wellsCoordinatesService.getWellCoordinates(COMPANY_NAME, null);
+        getTrueRopMetaCache().addClientListener(trueRopCacheListener);
+
         DailyOpsLoadConfig dailyOpsLoadConfig = notifyDPVAService.getDailyOpsLoadConfig(COMPANY_NAME);
         processPerformanceMapData(dailyOpsLoadConfig);
         log.info("Cache service end");
@@ -63,9 +68,6 @@ public class CacheService {
         notifyDPVAService.loadDPVAData(COMPANY_NAME, dailyOpsLoadConfig);
         getSurveyDataCache().addClientListener(surveyDataCacheListener);
         getPlanDataCache().addClientListener(wellPlanDataCacheListener);
-
-        wellsCoordinatesService.getWellCoordinates(COMPANY_NAME, null);
-        getTrueRopMetaCache().addClientListener(trueRopCacheListener);
     }
 
     private void processPerformanceMapData(DailyOpsLoadConfig dailyOpsLoadConfig) {
